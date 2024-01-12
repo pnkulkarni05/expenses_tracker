c0d6c75c25e8a72c44ce9047993f13bc178bec8b
Parth Kulkarni <pnkulkarni05@gmail.com>
2024-01-12 19:23:26 +0530

    Added a list of categories for the expenses in expense.dart using enum.

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,11 +2,18 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  Expense({required this.amount, required this.expense, required this.date})
-      : id = uuid.v4();
+  Expense({
+    required this.amount,
+    required this.expense,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
   final String id;
   final double amount;
   final String expense;
   final DateTime date;
+  final Category category;
 }
