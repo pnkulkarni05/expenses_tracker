88b6c3f34fbc3a0d173b6fe347dcaceac4cba8f6
Parth Kulkarni <pnkulkarni05@gmail.com>
2024-01-30 11:38:21 +0530

    Addded custome themes for cards, text and elevatedButton.

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,34 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 ColorScheme kColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 42, 50, 169));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 86, 96, 229));
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            foregroundColor: kColorScheme.primaryContainer,
-            backgroundColor: kColorScheme.onPrimaryContainer,
-          )),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          foregroundColor: kColorScheme.primaryContainer,
+          backgroundColor: kColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                color: kColorScheme.onSecondary,
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+              ),
+            ),
+      ),
       home: const Scaffold(
         body: Expenses(),
       ),
