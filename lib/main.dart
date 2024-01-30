4560fc91699bc58f62302e082408c4ba24f7584b
Parth Kulkarni <pnkulkarni05@gmail.com>
2024-01-30 11:18:23 +0530

    Added a custom colorScheme for the application in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+ColorScheme kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 42, 50, 169));
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            foregroundColor: kColorScheme.primaryContainer,
+            backgroundColor: kColorScheme.onPrimaryContainer,
+          )),
+      home: const Scaffold(
         body: Expenses(),
       ),
     ),
