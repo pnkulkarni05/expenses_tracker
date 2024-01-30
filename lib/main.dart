0bab693aa65c4fde87f3302092f73640229c6dcf
Parth Kulkarni <pnkulkarni05@gmail.com>
2024-01-30 16:56:02 +0530

    Write a code to lock the orientation of app by default using the services package of flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 ColorScheme kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
@@ -9,47 +10,51 @@ ColorScheme kDarkTheme = ColorScheme.fromSeed(
     brightness: Brightness.dark);
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkTheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkTheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkTheme.primaryContainer,
-          ),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          foregroundColor: kColorScheme.primaryContainer,
-          backgroundColor: kColorScheme.onPrimaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                color: kColorScheme.onSecondaryContainer,
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((fn) => runApp(
+            MaterialApp(
+              darkTheme: ThemeData.dark().copyWith(
+                colorScheme: kDarkTheme,
+                cardTheme: const CardTheme().copyWith(
+                  color: kDarkTheme.secondaryContainer,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                ),
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kDarkTheme.primaryContainer,
+                  ),
+                ),
+              ),
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData().copyWith(
+                colorScheme: kColorScheme,
+                appBarTheme: const AppBarTheme().copyWith(
+                  foregroundColor: kColorScheme.primaryContainer,
+                  backgroundColor: kColorScheme.onPrimaryContainer,
+                ),
+                cardTheme: const CardTheme().copyWith(
+                  color: kColorScheme.secondaryContainer,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                ),
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kColorScheme.primaryContainer,
+                  ),
+                ),
+                textTheme: ThemeData().textTheme.copyWith(
+                      titleLarge: TextStyle(
+                        color: kColorScheme.onSecondaryContainer,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+              ),
+              home: const Scaffold(
+                body: Expenses(),
               ),
             ),
-      ),
-      home: const Scaffold(
-        body: Expenses(),
-      ),
-    ),
-  );
+          ));
 }
