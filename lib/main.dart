29d534a33347352acf9de19843ac598867943db4
Parth Kulkarni <pnkulkarni05@gmail.com>
2024-01-30 13:05:48 +0530

    Worked on themes part to change themes according to light and dark mode of device system.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,33 @@ import 'package:flutter/material.dart';
 
 ColorScheme kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
+ColorScheme kDarkTheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 5, 99, 125),
+    brightness: Brightness.dark);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkTheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkTheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkTheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                color: kColorScheme.secondaryContainer,
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+            ),
+      ),
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
